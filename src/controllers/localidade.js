335f1d5787c0a3a27efcdf21129cc75636475325
joaoprates151 <joaoprates151@gmail.com>
2025-04-14 20:33:30 -0300

    INSERT LOCALIDADE

--- src/controllers/localidade.js
+++ src/controllers/localidade.js
@@ -42,11 +42,23 @@ module.exports = {
 
     async inserirLocalidades(request, response) {
         try {
+
+            const {NM_Localidade, UF_Localidade} = request.body
+
+            const sql = 'insert into localidade (NM_Localidade, UF_Localidade) values (?,?)'
+
+            const values = [NM_Localidade, UF_Localidade]
+            
+
+            const [results] = await db.query(sql, values)
+
+            const localidadeId = results.insertId
+
             return response.status(200).json({
 
                 sucesso: true,
-                mensagem: 'Inserir Localidades',
-                dados: null
+                mensagem: 'Localidades Inseridas',
+                dados: localidadeId
             });
         } catch (error) {
             return response.status(500).json({
