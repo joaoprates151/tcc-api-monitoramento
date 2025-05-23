1bf257a40c7b935300836073c5ee247fc2e45e0a
lidiasilva10 <lia611745@gmail.com>
2025-05-23 19:34:14 -0300

    colab. node
    colaborador concluído!

--- src/controllers/colaborador.js
+++ src/controllers/colaborador.js
@@ -25,10 +25,20 @@ module.exports ={
 
     async inserirColaborador(request, response){
         try {
+
+            const { Matricula, SN_Temporario, DT_Cadastro, ID_Pessoa, ID_Funcao, ID_Usuario_Cadastro}= request.body
+            const sql= 'INSERT INTO COLABORADOR (Matricula, SN_Temporario, DT_Cadastro, ID_Pessoa, ID_Funcao,ID_Usuario_Cadastro) value (?,?,?,?,?,?)';
+
+            const values = [Matricula, SN_Temporario, DT_Cadastro, ID_Pessoa, ID_Funcao, ID_Usuario_Cadastro]
+            
+            const [ressults] = await db.query(sql, values);
+
+            const colaborador_id = ressults.insertId
+
             return response.status(200).json({
                 sucesso: true,
                 mensagem: 'Inserir Colaborador.',
-                dados:null
+                dados:colaborador_id
             });
         }catch (error) {
             return response.status(500).json({
@@ -41,10 +51,21 @@ module.exports ={
 
     async atualizarColaborador(request, response){
         try {
+
+                    const { Matricula, SN_Temporario, DT_Cadastro, ID_Pessoa, ID_Funcao,ID_Usuario_Cadastro}= request.body
+        
+                    const { id } = request.params;
+        
+                    const sql= 'UPDATE usuario SET  Matricula =?, SN_Temporario =?, DT_Cadastro =?, ID_Pessoa =?, ID_Funcao =?,ID_Usuario_Cadastro =? WHERE ID_Colaborador = ?';
+                
+                    const values = [ Matricula, SN_Temporario, DT_Cadastro, ID_Pessoa, ID_Funcao,ID_Usuario_Cadastro, id]
+        
+                    const atualizarDados = await db.query(sql, values);
+        
             return response.status(200).json({
                 sucesso: true,
-                mensagem: 'Atualizar Colaborador.',
-                dados:null
+                mensagem: `Colaborador ${id} Atualização de Colaboradores!`,
+                dados: atualizarDados[0].affectedRows
             });
         }catch (error) {
             return response.status(500).json({
@@ -57,6 +78,19 @@ module.exports ={
 
     async excluirColaborador(request, response){
         try {
+            const { id } = request.params;
+            const sql= 'DELETE FROM colaborador WHERE ID_Colaborador = ?';
+            const values = [ id ]
+            const [result] = await db.query(sql, values);
+
+            if (result.affectedRows === 0) {
+                return response.status(404).json({
+                    sucesso: false,
+                    mensagem: 'Colaborador não encontrado.',
+                    dados: null
+                });
+            }
+
             return response.status(200).json({
                 sucesso: true,
                 mensagem: 'Excluir Colaborador.',
